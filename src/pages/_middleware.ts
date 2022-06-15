43b0fb2774a17f26575e1efd3f6ce9dbb9f6c3a1
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-06-15 10:29:03 -0400

    home route created

---
+++ src/pages/_middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export default function middleware(req: NextRequest) {
+	// clone request url
+	const url = req.nextUrl.clone();
+
+	// get pathname of request url '/example-pathname'
+	const { pathname } = req.nextUrl;
+
+	// get hostname of request 'demo.alpost.org', 'demo.localhost:3000'
+	const hostname = req.headers.get("host");
+
+	if (!hostname) {
+		return new Response(null, {
+			status: 400,
+			statusText: "No hostname found in request headers",
+		});
+	}
+
+	if (hostname === "localhost:3000" || hostname === "alpost.org") {
+		url.pathname = `/home${pathname}`;
+		return NextResponse.rewrite(url);
+	}
+}
