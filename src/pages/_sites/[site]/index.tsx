bdc59ba66e33f59008de643fd2f0b103b44bbf82
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-06-16 13:25:58 -0400

    ?.name

--- src/pages/_sites/[site]/index.tsx
+++ src/pages/_sites/[site]/index.tsx
@@ -12,7 +12,7 @@ interface IndexProps {
 }
 
 export default function Index(props: IndexProps) {
-	console.log(props.site.name);
+	console.log(props.site?.name);
 	return (
 		<div>
 			<h1>hey world</h1>
