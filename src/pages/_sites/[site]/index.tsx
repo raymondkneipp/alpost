e40da93019348e16d5350eef1f550ab5162d383a
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-06-16 12:45:20 -0400

    get data from subdomain

--- src/pages/_sites/[site]/index.tsx
+++ src/pages/_sites/[site]/index.tsx
@@ -1,7 +1,66 @@
-export default function Index() {
+import { GetStaticPaths, GetStaticProps } from "next";
+import { ParsedUrlQuery } from "querystring";
+import { prisma } from "@/db/client";
+
+interface PathProps extends ParsedUrlQuery {
+	site: string;
+}
+
+interface IndexProps {
+	stringifiedData: string;
+}
+
+export default function Index({ stringifiedData }: IndexProps) {
+	console.log(stringifiedData);
 	return (
 		<div>
 			<h1>site</h1>
 		</div>
 	);
 }
+
+export const getStaticPaths: GetStaticPaths<PathProps> = async () => {
+	const subdomains = await prisma.site.findMany({
+		select: {
+			subdomain: true,
+		},
+	});
+
+	const allPaths = [...subdomains.map(({ subdomain }) => subdomain)].filter(
+		(path) => path
+	) as Array<string>;
+
+	return {
+		paths: allPaths.map((path) => ({
+			params: {
+				site: path,
+			},
+		})),
+		fallback: true,
+	};
+};
+
+export const getStaticProps: GetStaticProps<IndexProps, PathProps> = async ({
+	params,
+}) => {
+	if (!params) throw new Error("No path parameters found");
+
+	const { site } = params;
+
+	const data = await prisma.site.findUnique({
+		where: {
+			subdomain: site,
+		},
+	});
+
+	if (!data) {
+		return { notFound: true, revalidate: 10 };
+	}
+
+	return {
+		props: {
+			stringifiedData: JSON.stringify(data),
+		},
+		revalidate: 3600,
+	};
+};
