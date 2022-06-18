0384b7e2d9bd39716ea6d1f1a8f37ea072e50f11
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-06-18 17:57:08 -0400

    restructured folders

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,7 +4,6 @@ export { default as Text } from "./Text";
 export { DList, DItem } from "./DList";
 export { List, Item } from "./List";
 export { default as Container } from "./Container";
-export { default as Footer } from "./Footer";
 export { default as Tall } from "./Tall";
 
 // Home components
@@ -14,3 +13,4 @@ export { default as Pricing } from "./home/Pricing";
 export { default as Hero } from "./sites/Hero";
 export { default as Navbar } from "./sites/Navbar";
 export { default as NavLink } from "./sites/NavLink";
+export { default as Footer } from "./sites/Footer";
