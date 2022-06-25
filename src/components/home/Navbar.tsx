d344c9c9c73d37cf81bb9189ee9b6b0f5922ee22
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-06-25 14:14:29 -0400

    removed font backups

--- src/components/home/Navbar.tsx
+++ src/components/home/Navbar.tsx
@@ -2,12 +2,13 @@ import Image from "next/image";
 import { Text, Button, Container } from "@/components";
 import getTextColor from "@/utils/get-text-color";
 import useTheme from "store/theme";
+import { NavLink } from "../sites";
 
 const Navbar: React.FC = () => {
 	const { color } = useTheme();
 	return (
 		<nav>
-			<Container className="flex items-center justify-between space-x-8">
+			<Container className="flex items-center justify-between space-x-12">
 				<div className="flex items-center space-x-2">
 					<Image src="/favicon.svg" width={60} height={60} layout="fixed" />
 					<Text variant="h5" size="sm" element="span">
@@ -15,8 +16,13 @@ const Navbar: React.FC = () => {
 						<span className={getTextColor(color)}>Post</span>
 					</Text>
 				</div>
-				<div className="flex-1"></div>
-				<div>
+				<div className="flex-1 flex space-x-4">
+					<NavLink href="/#features">Features</NavLink>
+					<NavLink href="/#testimonials">Testimonials</NavLink>
+					<NavLink href="/#pricing">Pricing</NavLink>
+				</div>
+				<div className="flex space-x-4">
+					<NavLink href="/login">Sign In</NavLink>
 					<Button href="/login">Get Started</Button>
 				</div>
 			</Container>
