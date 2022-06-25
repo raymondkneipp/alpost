e5502e3b251729bb62f6946e4f30f0d9529eaf18
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-06-25 14:20:58 -0400

    trying to get font to work in prod

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,12 +5,12 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					{/* <link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link
 						rel="preconnect"
 						href="https://fonts.gstatic.com"
 						crossOrigin="true"
-					/>
+					/> */}
 					<link
 						href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500&display=swap"
 						rel="stylesheet"
