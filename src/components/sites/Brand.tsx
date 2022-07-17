5c9572b1768171a52cda41e8e914c8f5745b0422
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-07-17 10:28:14 -0400

    added link to brand component

--- src/components/sites/Brand.tsx
+++ src/components/sites/Brand.tsx
@@ -1,15 +1,16 @@
 import { AmericanLegionLogo } from '@/components/shared';
 import { GeneralContext } from '@/contexts/sites';
+import Link from 'next/link';
 import { useContext } from 'react';
 
 const Brand: React.FC = () => {
 	const { subdomain } = useContext(GeneralContext);
 
 	return (
-		<div className="flex items-center">
+		<Link href="/" className="flex items-center">
 			<AmericanLegionLogo />
 			<span className="text-xl">{subdomain}</span>
-		</div>
+		</Link>
 	);
 };
 
