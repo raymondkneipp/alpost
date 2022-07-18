e67ba3a5a91a203f09ab9c87527a0677d8dd61c6
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-07-18 19:02:50 -0400

    copy change

--- src/components/sites/CTA.tsx
+++ src/components/sites/CTA.tsx
@@ -14,8 +14,8 @@ const CTA: React.FC = () => {
 					Join Our Post Today!
 				</h2>
 				<p className="md:text-lg text-neutral-100 dark:text-neutral-300">
-					Become a member of our post and be a part of the largest veteran
-					organization reaching over two million members.
+					Join our post and get involved with the nation's largest organization
+					for veterans of all ages.
 				</p>
 				<ButtonLink href="/apply" theme="light" size="lg">
 					Become a Member
