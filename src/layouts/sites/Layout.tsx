264414ed6cf826c2d9e36d4604b41ffb45df79ff
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-07-19 12:59:25 -0400

    more additions to next seo

--- src/layouts/sites/Layout.tsx
+++ src/layouts/sites/Layout.tsx
@@ -8,7 +8,6 @@ import {
 } from '@/contexts/sites';
 import { Footer, Navbar } from '@/components/sites';
 
-import Head from 'next/head';
 import { NewsProvider } from 'contexts/sites/NewsContext';
 import { NextSeo } from 'next-seo';
 
@@ -26,22 +25,39 @@ type Props = {
 const Layout: React.FC<Props> = ({ children, data }) => {
 	return (
 		<>
-			<Head>
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link
-					rel="preconnect"
-					href="https://fonts.gstatic.com"
-					crossOrigin="true"
-				/>
-				<link
-					href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500&display=swap"
-					rel="stylesheet"
-				/>
-			</Head>
 			<NextSeo
 				title="Home"
 				titleTemplate={`${data.name} American Legion Post ${data.subdomain} | %s`}
 				description={data.description}
+				additionalLinkTags={[
+					{
+						rel: 'preconnect',
+						href: 'https://fonts.googleapis.com',
+					},
+					{
+						rel: 'preconnect',
+						href: 'https://fonts.gstatic.com',
+						crossOrigin: 'true',
+					},
+					{
+						rel: 'stylesheet',
+						href: 'https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500&display=swap',
+					},
+				]}
+				openGraph={{
+					type: 'website',
+					url: 'https://www.example.com/page',
+					title: `${data.name} American Legion Post ${data.subdomain}`,
+					description: `${data.description}`,
+					images: [
+						{
+							url: 'https://alpost.org/shared/american-flag.jpg',
+							width: 800,
+							height: 600,
+							alt: 'American Flag',
+						},
+					],
+				}}
 			/>
 			<GeneralProvider data={data}>
 				<ThemeProvider data={data.theme}>
