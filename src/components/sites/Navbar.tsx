6910d306e5a767d083b50958fd077edd8e757d0e
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-07-19 13:09:23 -0400

    sticky navbar

--- src/components/sites/Navbar.tsx
+++ src/components/sites/Navbar.tsx
@@ -1,15 +1,16 @@
-import { Brand, NavLink, MobileNav } from '@/components/sites';
+import { Brand, MobileNav, NavLink } from '@/components/sites';
+
 import { Container } from '@/components/shared';
 
 const Navbar: React.FC = () => {
 	return (
-		<nav className="z-50 fixed inset-x-0 top-0 border-b border-neutral-200 dark:border-neutral-800 py-2 bg-neutral-100 dark:bg-neutral-900">
+		<nav className="sticky inset-x-0 top-0 z-50 py-2 border-b border-neutral-200 dark:border-neutral-800 bg-neutral-100 dark:bg-neutral-900">
 			<Container className="flex items-center justify-between">
 				<Brand />
 
 				<MobileNav />
 
-				<div className="hidden items-center md:space-x-2 lg:space-x-4 sm:flex">
+				<div className="items-center hidden md:space-x-2 lg:space-x-4 sm:flex">
 					<NavLink href="/">Home</NavLink>
 					<NavLink href="/about">About</NavLink>
 					<NavLink href="/news">News</NavLink>
