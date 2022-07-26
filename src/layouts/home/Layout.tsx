b4141faf0b003b140bca8cf9f57ab49bf0c6abde
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-07-26 11:55:28 -0400

    basic home page

--- src/layouts/home/Layout.tsx
+++ src/layouts/home/Layout.tsx
@@ -1,9 +1,35 @@
+import { NextSeo } from 'next-seo';
+
 type Props = {
 	children: React.ReactNode;
 };
 
 const Layout: React.FC<Props> = ({ children }) => {
-	return <div className="border-8 border-red-500">{children}</div>;
+	return (
+		<>
+			<NextSeo
+				title="Home"
+				titleTemplate="American Legion Website Builder | alpost | %s"
+				description=""
+				additionalLinkTags={[
+					{
+						rel: 'preconnect',
+						href: 'https://fonts.googleapis.com',
+					},
+					{
+						rel: 'preconnect',
+						href: 'https://fonts.gstatic.com',
+						crossOrigin: 'true',
+					},
+					{
+						rel: 'stylesheet',
+						href: 'https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500&display=swap',
+					},
+				]}
+			/>
+			<main className="bg-blue-900">{children}</main>
+		</>
+	);
 };
 
 export default Layout;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,22 @@
-import type { NextPage } from 'next';
-import { Layout } from '@/layouts/home';
 import { Container } from '@/components/shared';
+import { Layout } from '@/layouts/home';
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
 	return (
 		<Layout>
 			<Container>
-				<h1 className="text-red-500">home</h1>
+				<span className="text-2xl font-medium text-red-300 sm:text-4xl font-heading">
+					alpost
+				</span>
+				<div className="bg-blue-900">
+					<p className="text-red-300">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis non
+						totam optio neque dolore dolores hic! Architecto dolorem magnam
+						debitis eveniet maxime, autem dolores eius, dolore facilis rerum
+						soluta assumenda.
+					</p>
+				</div>
 			</Container>
 		</Layout>
 	);
